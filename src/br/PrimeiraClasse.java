0d3b8ee58b634a6a581bae972963371a75c62b32
Lorenzo <lorenzo.202212195@unilasalle.edu.br>
2025-01-20 12:56:06 -0300

    alteracoes

---
+++ src/br/PrimeiraClasse.java
@@ -0,0 +1,11 @@
+void main Quarto
+Ao acordar, abrir ‘’olhos’’
+    Se ‘’olhos’’ == ‘‘abertos’’
+        Levantar-se da cama e ir ao banheiro
+            Retornar ao Quarto, 
+Ligar 'computador'
+    Se ‘’computador’’ = ligado
+Tentar ‘’curso EBAC’’
+Retornar para cozinha pegar água	Ver aulas
+Se ‘’tempo de aula’’ =+ ‘’2’’ horas
+	Almoçar
